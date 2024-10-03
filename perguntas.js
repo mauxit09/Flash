50a8b9560fc1b20b069ae0942b42fc57a496f69a
mauxit09 <148362415+mauxit09@users.noreply.github.com>
2024-10-03 14:57:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao (
+    'Corinthians',
+    'Quem deu o nome do Corinthians ?',
+    'Sport Club Corinthians Paulista foi o nome sugerido por Joaquim Ambrósio.'
+)
+criaCartao (
+    'Corinthians',
+   'O que representa o Corinthians ?',
+    'O nome do Corinthians tem uma origem inspiradora e está diretamente relacionado à admiração de seus fundadores por um clube inglês.'
+)
+
+criaCartao (
+    'Corinthians',
+   'Quem é o rival do Corinthians ?',
+    'Seus rivais históricos são o Palmeiras, com quem disputa o Derby Paulista; o São Paulo, com quem disputa o Majestoso; e o Santos, com quem disputa o Clássico Alvinegro.'
+)
+criaCartao (
+    'Corinthians',
+   'O que significam as 5 estrelas do Corinthians ?',
+    'Quatro servem para lembrar cada título brasileiro conquistado pelo time, enquanto a maior delas, mais ao alto, destaca o Mundial de Clubes da Fifa, obtido pela equipe em 2000.'
+)
+
